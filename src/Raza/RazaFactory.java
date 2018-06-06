6f72689c7657e45c0272f9aaca709bf8a3d7ccb2
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2018-06-05 23:26:23 -0600

    creando la factory

---
+++ src/Raza/RazaFactory.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Raza;
+
+import Edificacion.Edificacion;
+import Jugador.Jugador;
+import Jugador.Jugador1;
+import Jugador.Jugador2;
+import Milicia.Milicia;
+import Vehiculo.Vehiculo;
+import estherlara.sworld.AbstractFactory;
+
+/**
+ *
+ * @author User
+ */
+public class RazaFactory implements AbstractFactory {
+
+    @Override
+    public Jugador getJugador(int type) {
+        return null;
+    }
+
+    @Override
+    public Edificacion getEdificacion(int type) {
+        return null;
+    }
+
+    @Override
+    public Raza getRaza(int type) {
+        switch (type) {
+            case 1:
+                return new Soldados();
+            case 2:
+                return new Duende();
+
+        }
+        return null;
+    }
+
+    @Override
+    public Vehiculo getVehiculo(int type) {
+        return null;
+    }
+
+    @Override
+    public Milicia getMilicia(int type) {
+        return null; 
+    }
+    
+}
