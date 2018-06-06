8ec46b75e4b7a87d7dee788d1587ba3f97b8146b
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2018-06-05 23:26:48 -0600

    implementando la inferface

---
+++ src/Raza/Soldados.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Raza;
+
+/**
+ *
+ * @author User
+ */
+public class Soldados implements Raza {
+
+    @Override
+    public void elegir() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
