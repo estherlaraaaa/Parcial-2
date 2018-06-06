90a14e48bb26191e1925b52fdb75f811859075b6
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2018-06-05 23:20:15 -0600

    creando la interface de edificacion

---
+++ src/Edificacion/Edificacion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Edificacion;
+
+/**
+ *
+ * @author User
+ */
+public interface Edificacion {
+    void crear(int idjugador); 
+    void mostrar(int id); 
+    
+}
