c9d8972ee5d1d2e4ea29c9924d62f33aee30322f
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2018-06-05 23:23:21 -0600

    creando interface

---
+++ src/Jugador/Jugador.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jugador;
+
+/**
+ *
+ * @author User
+ */
+public interface Jugador {
+    void registrar(int id, String nombre, int raza); 
+    void mostrarRecursos(int id);
+    
+}
