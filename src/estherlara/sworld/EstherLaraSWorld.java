1161ba54e44420edbae9967cae270a4ae84fdf52
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2018-06-05 23:28:18 -0600

    main

---
+++ src/estherlara/sworld/EstherLaraSWorld.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estherlara.sworld;
+
+
+
+/**
+ *
+ * @author User
+ */
+public class EstherLaraSWorld {
+
+
+    public static void main(String[] args) {
+        
+        MenuRegistrar registro = new MenuRegistrar (); 
+        
+        registro.Registro();
+
+        
+    }
+    
+}
