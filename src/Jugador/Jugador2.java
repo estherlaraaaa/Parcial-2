ff28a837375ddfc134aa440f1ead032c35751567
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2018-06-05 23:24:48 -0600

    implementando la interface

---
+++ src/Jugador/Jugador2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jugador;
+
+import static Jugador.ConstructorLista.listaJugador;
+
+/**
+ *
+ * @author User
+ */
+public class Jugador2 implements Jugador {
+
+    @Override
+    public void registrar(int id, String nombre, int raza) {
+        listaJugador.Agregar(id, nombre, raza, 10000, 10000, 10);
+    }
+
+    @Override
+    public void mostrarRecursos(int id) {
+        listaJugador.Mostrar(id);
+    }
+
+
+    
+}
