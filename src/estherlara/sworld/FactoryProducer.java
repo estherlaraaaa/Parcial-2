7ac480dd1305633acee79b0414b1cdc7f220ea10
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2018-06-14 23:07:34 -0600

    A traves del factory producer se puede acceder a cada uno de los factorys de los diferentes paquetes

--- src/estherlara/sworld/FactoryProducer.java
+++ src/estherlara/sworld/FactoryProducer.java
@@ -11,28 +11,36 @@ import Jugador.JugadorFactory;
 import Milicia.MiliciaFactory;
 import Raza.RazaFactory;
 import Vehiculo.VehiculoFactory;
+import java.util.InputMismatchException;
 
 /**
  *
  * @author User
  */
 public interface FactoryProducer {
-    public static AbstractFactory getFactory(int type){
-        switch (type){
-            case 1:
-                return  new JugadorFactory();
-            case 2:
-                return new Centro_de_MandoFactory();
-            case 3:
-                return new EdificacionFactory();
-            case 4:
-                return new RazaFactory();
-            case 5:
-                return new VehiculoFactory();
-            case 6:
-                return new MiliciaFactory();
+
+    public static AbstractFactory getFactory(int type) {
+        try {
+            switch (type) {
+                case 1:
+                    return new JugadorFactory();
+                case 2:
+                    return new Centro_de_MandoFactory();
+                case 3:
+                    return new EdificacionFactory();
+                case 4:
+                    return new RazaFactory();
+                case 5:
+                    return new VehiculoFactory();
+                case 6:
+                    return new MiliciaFactory();
+            }
+            return null;
+        } catch (InputMismatchException e) {
+            System.err.println("Por favor, ingrese una de las opciones.");
+
         }
         return null;
     }
-    
+
 }
