182148a9e216481e7e917dcec6abfdc4b2c51915
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2018-06-14 23:10:10 -0600

    Se crea el menu registrar, en donde solo contiene un menu sencillo para que el usuario se regstre, asi mismo se manda a llamar el menu general que es el que contiene todo(lo principal). Asi mismo se manda a lllamr de la libreria la funcion random ya que se utilizo para ver que jugador iniciaba primero

--- src/estherlara/sworld/MenuRegistrar.java
+++ src/estherlara/sworld/MenuRegistrar.java
@@ -6,7 +6,6 @@
 package estherlara.sworld;
 
 import Centro_de_Mando.Centro_de_Mando;
-import Edificacion.Edificacion;
 import Jugador.Jugador;
 import java.util.Random;
 import java.util.Scanner;
@@ -15,28 +14,29 @@ import java.util.Scanner;
  *
  * @author User
  */
-public class MenuRegistrar {
+public class MenuRegistrar  {
 
     Scanner leer = new Scanner(System.in);
 
     AbstractFactory factory;
 
-    public void Registro() {
-
+    public void Registro() throws Exception{
+        
         for (int i = 1; i <= 2; i++) {
             String nombre = "";
             int raza = 0;
 
             System.out.println("\033[36m----------REGISTRANDO JUGADOR " + i + "--------------");
-            System.out.println("\033[35mIngrese su nombre por favor");
+            System.out.println("\033[34mINGRESE SU NOMBRE POR FAVOR");
             System.out.println("   ");
             nombre = leer.nextLine();
             System.out.println("  ");
-            System.out.println("\033[35mCon qué raza desea jugar: \n1.Soldados. \n2.Duende. \n3.Extraterrestres.");
+            System.out.println("\033[35m¿CON QUÉ RAZA DESEA JUGAR? \n1.Soldados. \n2.Duende. \n3.Extraterrestres.");
             System.out.println("  ");
             raza = leer.nextInt();
             leer.nextLine();
             System.out.println("  ");
+            
 
             factory = FactoryProducer.getFactory(1);
             Jugador jugador = factory.getJugador(i);
@@ -45,15 +45,17 @@ public class MenuRegistrar {
             factory = FactoryProducer.getFactory(2);
             Centro_de_Mando CM = factory.getCentro_De_Mando(i);
             CM.crear();
+            
+          
         }
-
+        
         MenuGeneral general = new MenuGeneral();
 
         Random generadorAleatorios = new Random();
  
         int random = 1 + generadorAleatorios.nextInt(2);
         
-        general.SubMenu(random);
+        general.SubMenu(random,1 );
 
     }
 
